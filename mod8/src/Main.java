52399f1f83f23fd94387577a2ce240803ce8a7a7
caroldf07 <carolina.df@outlook.com>
2022-02-01 17:29:45 -0300

    ativadade módulo 8

---
+++ mod8/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author carolinafonseca
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        TarefaCalculoMedia aluno1 = new TarefaCalculoMedia();
+        TarefaCalculoMedia aluno2 = new TarefaCalculoMedia();
+        aluno1.Media(10, 5, 3, 1);
+        aluno2.Media(4, 5, 5, 10);
+    }
+}
