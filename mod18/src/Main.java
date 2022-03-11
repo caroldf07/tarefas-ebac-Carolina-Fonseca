f36234d346e134dc5df00863ef5e7e8eddd56e4c
caroldf07 <carolina.df@outlook.com>
2022-03-11 16:23:58 -0300

    atividade mod 20 - annotation e reflection

--- mod18/src/Main.java
+++ mod18/src/Main.java
@@ -1,15 +1,49 @@
+import java.lang.reflect.*;
+
 /**
  * @author carolinafonseca on 23/02/2022
- * @project tarefas-ebac-Carolina-Fonseca Criação de annotation referente ao módulo 18 - Annotations
- * e Exceptions
+ * @project tarefas-ebac-Carolina-Fonseca Criação de annotation referente ao módulo 20 - Annotations
+ * e Reflections
  */
 
 public class Main {
     @Tabela(nomeTabela = "tabelinha")
-    static private String nome;
+    public String nome;
+
 
     public static void main(String[] args) {
-        System.out.println(nome);
+        /*
+         * Monta o objeto com a classe que recebe a annotation
+         */
+        Class<?> clazz = Main.class;
 
+        {
+            try {
+                /*
+                 * Monta o objeto construtor da classe que possui a annotation
+                 */
+                Constructor<?> obj = clazz.getConstructor();
+
+            /*
+            Instancia um novo objeto da classe refenciada anteriormente, utilizando o construtor montado
+             */
+                Main main = (Main) obj.newInstance();
+
+            /*
+            Puxa os campos criado dentro do objeto instanciado
+             */
+                Field[] fields = main.getClass().getDeclaredFields();
+
+                for (Field field : fields) {
+                    if (field.isAnnotationPresent(Tabela.class)) {
+                        String tabelaNome = field.getAnnotation(Tabela.class).nomeTabela();
+                        System.out.println(tabelaNome);
+                    }
+                }
+            } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
