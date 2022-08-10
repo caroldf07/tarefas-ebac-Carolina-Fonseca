de5b8e766daa6ec9d4441788301ea89c62169a56
caroldf07 <carolina.df@outlook.com>
2022-08-10 20:54:36 -0300

    Início projeto 03 - parte 02 e 03.

---
+++ Projeto3/src/dao/generic/jdbc/dao/ClienteDao.java
@@ -0,0 +1,19 @@
+package dao.generic.jdbc.dao;
+
+import domain.Cliente;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ClienteDao {
+    Integer cadastrar(Cliente cliente) throws SQLException;
+
+    Integer atualizar(Cliente cliente) throws SQLException;
+
+    Cliente buscar(String codigo) throws SQLException;
+
+    List<Cliente> buscarTodos() throws SQLException;
+
+    void excluir(Cliente cliente) throws SQLException;
+
+}
