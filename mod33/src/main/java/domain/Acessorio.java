f243606d3a3b4ddc5e28d399a9872f25d04168bb
carol <carolina.df@outlook.com>
2022-12-07 20:55:35 -0300

    Finalizado mod 33.

--- mod33/src/main/java/domain/Acessorio.java
+++ mod33/src/main/java/domain/Acessorio.java
@@ -1,4 +1,49 @@
 package main.java.domain;
 
-public class Acessorio {
+import main.java.dao.generic.Persistente;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_acessorio")
+public class Acessorio implements Persistente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
+    @SequenceGenerator(name = "produto_seq", sequenceName = "sq_produto", initialValue = 10,
+            allocationSize = 1)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    @Column(length = 100, nullable = false)
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_carro_fk",
+            foreignKey = @ForeignKey(name = "fk_carro_acessorio"),
+            referencedColumnName = "id", nullable = false
+    )
+    private Carro carro;
+
+
+    public Acessorio(String nome, String descricao, Carro carro) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.carro = carro;
+    }
+
+    @Deprecated
+    public Acessorio() {
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
