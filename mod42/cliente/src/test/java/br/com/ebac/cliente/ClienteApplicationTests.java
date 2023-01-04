b1a80987fc311c6d315435b04970e1ef837b51be
caroldf07 <carolina.df@outlook.com>
2023-01-04 16:43:31 -0300

    mod 42  - iniciado

---
+++ mod42/cliente/src/test/java/br/com/ebac/cliente/ClienteApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.ebac.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteApplicationTests {
+
+}
---
+++ mod42/produto/src/test/java/br/com/ebac/produto/ProdutoApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.ebac.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+}
