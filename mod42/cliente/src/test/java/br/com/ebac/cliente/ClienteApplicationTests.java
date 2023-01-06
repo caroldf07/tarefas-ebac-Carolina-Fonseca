cc60ca3d95adf2a8f108398aed0558219c6c19f4
caroldf07 <carolina.df@outlook.com>
2023-01-06 14:32:39 -0300

    mod 42  - finalizado.

--- mod42/cliente/src/test/java/br/com/ebac/cliente/ClienteApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package br.com.ebac.cliente;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ClienteApplicationTests {
-
-}
